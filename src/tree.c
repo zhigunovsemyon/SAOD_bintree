ce01df1d1f81bff9afcba7682592620b5a6384b9
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-02-15 12:53:51 +0300

    Написал (?) поиск

--- src/tree.c
+++ src/tree.c
@@ -1,24 +1,24 @@
 #include "tree.h"
 #include <malloc.h> /*malloc*/
 
-/*Инициализация дерева. Принимает функцию распределения и размер данных*/
-Tree * TreeInit(size_t esize, int (*compar)(void const *, void const *))
+static struct TreeNode **
+TreeFind_(struct TreeNode ** tree, compar_fn compar, void const * key)
 {
-	if (esize == 0 || compar == NULL)
-		return NULL;
+	if (*tree == NULL)
+		return tree;
 
-	Tree * pTree = (Tree *)malloc(sizeof (Tree));
-	if (!pTree)
-		return NULL;
+	/*Сохранение результата сравения*/
+	int cmp_res = compar(key, *tree);
 
-	pTree->esize = esize;
-	pTree->compar = compar;
-	pTree->root = NULL;
-
-	return pTree;
+	if (cmp_res == 0)
+		return tree;
+	/*Если ключ меньше ячейки, поиск продолжается в левой части,
+	 * если больше -- в правой*/
+	return TreeFind_(cmp_res < 0 ? &((*tree)->l) : &((*tree)->r), compar,
+			 key);
 }
 
-static void TreeFree_ (struct TreeNode ** node)
+static void TreeFree_(struct TreeNode ** node)
 {
 	/*Если передана пустая ячейка*/
 	if ((*node) == NULL)
@@ -35,27 +35,35 @@ static void TreeFree_ (struct TreeNode ** node)
 	TreeFree_(&((*node)->r));
 }
 
+/*Инициализация дерева. Принимает функцию распределения и размер данных*/
+Tree * TreeInit(size_t esize, int (*compar)(void const *, void const *))
+{
+	if (esize == 0 || compar == NULL)
+		return NULL;
+
+	Tree * pTree = (Tree *)malloc(sizeof(Tree));
+	if (!pTree)
+		return NULL;
+
+	pTree->esize = esize;
+	pTree->compar = compar;
+	pTree->root = NULL;
+
+	return pTree;
+}
+
 /*Уничтожение дерева*/
-void TreeFree(Tree *pTree)
+void TreeFree(Tree * pTree)
 {
 	TreeFree_(&(pTree->root));
 	free(pTree);
 }
 
 /*Вставка элемента данных*/
-int TreeInsert(Tree * pTree, void * const src)
-{
-
-}
+int TreeInsert(Tree * pTree, void * const src) {}
 
 /*Удаление элемента с переданным ключом*/
-int TreeRemove(Tree * pTree, void * const key)
-{
-
-}
+int TreeRemove(Tree * pTree, void * const key) {}
 
 /*Копирование элемента по ключу key из дерева в dest*/
-int TreeCopy(Tree * pTree, void * const key, void * dest)
-{
-
-}
+int TreeCopy(Tree * pTree, void * const key, void * dest) {}
